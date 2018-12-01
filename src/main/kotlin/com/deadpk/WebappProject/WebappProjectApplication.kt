5fb921a04c2f3378186363b7de40dd99afe94b78
Sangmin Park <deadpk.g@gmail.com>
2018-12-01 11:24:01 +0900

    Initial commit

---
+++ src/main/kotlin/com/deadpk/WebappProject/WebappProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.deadpk.WebappProject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebappProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebappProjectApplication>(*args)
+}
