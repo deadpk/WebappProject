f109d137bcd31cef28b1a849e09d2e89c620db52
deadpk <deadpk.g@gmail.com>
2018-12-23 22:16:57 +0900

    FEATURE-2 TEST 코드 추가

--- src/test/kotlin/com/deadpk/WebappProject/WebappProjectApplicationTests.kt
+++ src/test/kotlin/com/deadpk/WebappProject/WebappProjectApplicationTests.kt
@@ -1,16 +1,25 @@
 package com.deadpk.WebappProject
 
+import com.deadpk.WebappProject.web.WebRestController
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@WebMvcTest(WebRestController::class)
 class WebappProjectApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+	@Autowired
+	lateinit var mock: MockMvc
 
+	@Test
+	fun Hello() {
+		mock.perform(get("/hello")).andExpect(content().string("hello"))
+	}
 }
